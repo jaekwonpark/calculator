65b22288c65336fa2b4052a83ec2e42e7c5e580d
japark <japark@zynga.com>
2015-05-19 00:12:01 -0700

    Initial commit

---
+++ Calculator.java
@@ -0,0 +1,8 @@
+public class Calculator {
+
+	public static void main(String[] args) {
+		int value = 0;
+		System.out.printf("Input = %s\n", args[0]);
+		System.out.printf("Result = %s\n", Integer.toString(value));
+	}
+}
